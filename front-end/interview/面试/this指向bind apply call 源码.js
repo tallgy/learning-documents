29807a963a5f8b7867b650ada59b2b144eeab0d1
tallgy <1776232657@qq.com>
2021-09-07 22:57:48 +0800

    面试  bind, apply, call的源码实现

---
+++ front-end/interview/面试/this指向bind apply call 源码.js
@@ -0,0 +1,92 @@
+Function.prototype.myBind = function (ctx) {
+
+  ctx = ctx || window;
+  ctx.fn = this;
+  let arg = [...arguments].slice(1);
+
+  return function () {
+    ctx.fn(...arg);
+
+    delete ctx.fn;
+  };
+}
+
+// obj.myFun.bind(db, '成都','上海')();
+
+
+
+
+
+
+
+
+
+
+
+Function.prototype.myApply = function (ctx, arr) {
+  ctx = ctx || window;
+
+  ctx.fn = this;
+
+  let result;
+  if (!arr) {
+    result = ctx.fn();
+  } else {
+    if (!(arr instanceof Array)) throw new Error('params must be array');
+    result = ctx.fn(...arr);
+  }
+
+  delete ctx.fn;
+  return result;
+}
+
+
+
+
+
+
+
+
+Function.prototype.myCall = function (ctx) {
+  ctx = ctx || window;
+  ctx.fn = this;
+
+  let arg = [...arguments].slice(1);
+
+  let result;
+  result = ctx.fn(...arg);
+
+  delete ctx.fn;
+  return result
+}
+
+// obj.myFun.myCall(db, 'cc');
+
+
+
+
+
+
+
+var obj = {
+  name: '1',
+  myFun(fm) {
+    console.log(`${this.name} ------ ${fm}`);
+  }
+}
+
+var db = {
+  name: '2'
+}
+
+
+
+// obj.myFun.bind(db, '成都','上海')();
+// obj.myFun.myBind(db, '成都','上海')();
+
+
+// obj.myFun.apply(db,['成都','上海']);
+// obj.myFun.myApply(db,['成都','上海']);
+
+// obj.myFun.call(db, 'cc', 'dd');
+// obj.myFun.myCall(db, 'cc', 'dd');
